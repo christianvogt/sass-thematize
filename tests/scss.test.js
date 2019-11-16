8b6221a71b89a200f100edea44fe0e785105b57a
christianvogt <cvogt@redhat.com>
2019-11-15 22:35:45 -0500

    Update SASS True test API

--- tests/scss.test.js
+++ tests/scss.test.js
@@ -1,14 +1,11 @@
-const glob = require('glob');
-const path = require('path');
-const sassTrue = require('sass-true');
+const glob = require("glob");
+const path = require("path");
+const sassTrue = require("sass-true");
 
-describe('SCSS', () => {
-  // Find all of the SCSS files that end in `*.spec.scss` in any directory in this project.
-  // Use path.resolve because True requires absolute paths to compile test files.
-  const sassTestFiles = glob.sync(path.resolve(__dirname, '../**/*.test.scss'));
+console.log(describe);
+// Find all of the SCSS files that end in `*.spec.scss` in any directory in this project.
+// Use path.resolve because True requires absolute paths to compile test files.
+const sassTestFiles = glob.sync(path.resolve(__dirname, "../**/*.test.scss"));
 
-  // Run True on every file found with the describe and it methods provided.
-  sassTestFiles.forEach(file =>
-    sassTrue.runSass({ file }, describe, it)
-  );
-});
+// Run True on every file found with the describe and it methods provided.
+sassTestFiles.forEach(file => sassTrue.runSass({ file }, { describe, it }));
